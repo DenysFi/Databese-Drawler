1a9f861a24b6a6ffa0f1cea0c10820864492225c
DenysFi <figuradenis1@gmail.com>
2024-04-17 19:08:01 +0300

    corrected path calculation

--- src/utiles/calckPath.ts
+++ src/utiles/calckPath.ts
@@ -13,14 +13,16 @@ export function calcPath({ x1, x2, y1, y2, width }: ICalcPath) {
     if (Math.abs(y1 - y2) <= 30) {
         radius = Math.abs(y2 - y1) / 3;
         // если радиус оказался меньше двух, то рисовать полукруг нет смысла, и мы рисуем прямую линию
-        if (radius <= 2) {
+        if (radius <= 4) {
             if (x1 + width <= x2) return `M ${x1 + width} ${y1} L ${x2} ${y2 + 0.1}`;
             else if (x2 + width < x1)
                 return `M ${x1} ${y1} L ${x2 + width} ${y2}`;
         }
     }
+    //when start table is upside
     if (y1 < y2) {
         if (x1 + width < x2) {
+            //calc right side
             return `M ${x1 + width} ${y1} 
             L ${midX - radius} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 1 ${midX} ${y1 + radius} 
@@ -28,13 +30,15 @@ export function calcPath({ x1, x2, y1, y2, width }: ICalcPath) {
             A ${radius} ${radius} 0 0 0 ${midX + radius} ${y2} 
             L ${x2} ${y2}`;
         } else if (x1 >= x2 + width) {
+            //calc left side
             return `M ${x1} ${y1} 
             L ${midX + radius} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 0 ${midX} ${y1 + radius} 
             L ${midX} ${y2 - radius} 
             A ${radius} ${radius} 0 0 1 ${midX - radius} ${y2} 
-            L ${x2} ${y2}`;
+            L ${x2 + width} ${y2}`;
         } else if (x1 > x2 && x2 <= x1 + width) {
+            // left + under table
             const x2offset = x2 - 20;
             return `M ${x1} ${y1} 
             L ${x2offset} ${y1 + 0.1} 
@@ -43,16 +47,19 @@ export function calcPath({ x1, x2, y1, y2, width }: ICalcPath) {
             A ${radius} ${radius} 0 0 0 ${x2offset} ${y2} 
             L ${x2} ${y2}`;
         } else if (x2 > x1 && x2 <= x1 + width) {
+            // right + under table
             const x2offset = x2 + width + 20;
-            return `M ${x1} ${y1} 
+            return `M ${x1 + width} ${y1} 
             L ${x2offset} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 1 ${x2offset + radius} ${y1 + radius} 
             L ${x2offset + radius} ${y2 - radius} 
             A ${radius} ${radius} 0 0 1 ${x2offset} ${y2} 
-            L ${x2} ${y2}`;
+            L ${x2 + width} ${y2}`;
         }
     } else {
+        //when start table is downside
         if (x1 + width < x2) {
+            //calc right side
             return `M ${x1 + width} ${y1} 
             L ${midX - radius} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 0 ${midX} ${y1 - radius} 
@@ -60,13 +67,15 @@ export function calcPath({ x1, x2, y1, y2, width }: ICalcPath) {
             A ${radius} ${radius} 0 0 1 ${midX + radius} ${y2} 
             L ${x2} ${y2}`;
         } else if (x1 >= x2 + width) {
+            //calc left side
             return `M ${x1} ${y1} 
             L ${midX + radius} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 1 ${midX} ${y1 - radius} 
             L ${midX} ${y2 + radius} 
             A ${radius} ${radius} 0 0 0 ${midX - radius} ${y2} 
-            L ${x2} ${y2}`;
+            L ${x2 + width} ${y2}`;
         } else if (x1 > x2 && x2 <= x1 + width) {
+            // left + under table
             const x2offset = x2 - 20;
             return `M ${x1} ${y1} 
             L ${x2offset} ${y1 + 0.1} 
@@ -75,13 +84,14 @@ export function calcPath({ x1, x2, y1, y2, width }: ICalcPath) {
             A ${radius} ${radius} 0 0 1 ${x2offset} ${y2} 
             L ${x2} ${y2}`;
         } else if (x2 > x1 && x2 <= x1 + width) {
+            // right + under table
             const x2offset = x2 + width + 20;
-            return `M ${x1} ${y1} 
+            return `M ${x1 + width} ${y1} 
             L ${x2offset} ${y1 + 0.1} 
             A ${radius} ${radius} 0 0 0 ${x2offset + radius} ${y1 - radius} 
             L ${x2offset + radius} ${y2 + radius} 
             A ${radius} ${radius} 0 0 0 ${x2offset} ${y2} 
-            L ${x2} ${y2}`;
+            L ${x2 + width} ${y2}`;
         }
     }
 }
