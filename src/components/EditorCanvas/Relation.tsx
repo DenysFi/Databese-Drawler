20bb51e2ad00ba63e5b823d6bb2800a65dfd9842
DenysFi <figuradenis1@gmail.com>
2024-04-17 19:08:48 +0300

    added type of relation on canvas

--- src/components/EditorCanvas/Relation.tsx
+++ src/components/EditorCanvas/Relation.tsx
@@ -1,8 +1,9 @@
 import { tableDefaultRowHeight, tableDefaultWidth, tableHeaderHeight } from "@/Constants/constants";
+import { connectionType, objectType } from "@/Constants/enums";
 import { ITableRelation } from "@/Types/table";
 import { useAppSelector } from "@/redux-hooks";
 import { calcPath } from "@/utiles/calckPath";
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 interface IRelation {
     data: ITableRelation
 }
@@ -11,10 +12,35 @@ const Relation: FC<IRelation> = ({ data }) => {
     const { tables } = useAppSelector(state => state.tables);
     const table1 = tables.find(t => t.id === data.startTableId)!
     const table2 = tables.find(t => t.id === data.endTableId)!
+    const relationRef = useRef<SVGPathElement>(null);
+    const circleRadius = 10;
+    const [circlesCoords, setCirclesCoords] = useState({
+        x1: 0,
+        x2: 0,
+        y1: 0,
+        y2: 0
+    })
+    const [fromText, toText] = getRelationType(data.connectionName)
+    useEffect(() => {
+        if (!relationRef.current) return;
+
+        const totalLength = relationRef.current.getTotalLength();
+        console.log(totalLength);
+        const coord1 = relationRef.current.getPointAtLength(32);
+        const coord2 = relationRef.current.getPointAtLength(totalLength - 32);
+
+        setCirclesCoords({
+            x1: coord1.x,
+            x2: coord2.x,
+            y1: coord1.y,
+            y2: coord2.y
+        })
+    }, [tables])
 
     return (
         <g className="select-none">
             <path
+                ref={relationRef}
                 d={calcPath({
                     width: tableDefaultWidth,
                     x1: table1.x,
@@ -22,8 +48,25 @@ const Relation: FC<IRelation> = ({ data }) => {
                     y1: table1.y + tableHeaderHeight + ((data.startTableField * tableDefaultRowHeight) + tableDefaultRowHeight) - tableDefaultRowHeight / 2,
                     y2: table2.y + tableHeaderHeight + ((data.endTableField * tableDefaultRowHeight) + tableDefaultRowHeight) - tableDefaultRowHeight / 2
                 })} strokeWidth='2px' stroke="gray" fill="none" />
+            <circle cx={circlesCoords.x1} cy={circlesCoords.y1} r={circleRadius} stroke="none" fill="gray" />
+            <text x={circlesCoords.x1} y={circlesCoords.y1} fill="white" textAnchor="middle" dominantBaseline='middle'>{fromText}</text>
+            <circle cx={circlesCoords.x2} cy={circlesCoords.y2} r={circleRadius} stroke="none" fill="gray" />
+            <text x={circlesCoords.x2} y={circlesCoords.y2} fill="white" textAnchor="middle" dominantBaseline='middle'>{toText}</text>
         </g>
     );
 };
 
 export default Relation;
+
+function getRelationType(type: connectionType) {
+    switch (type) {
+        case connectionType.MANY_TO_MANY:
+            return ['n', 'n']
+        case connectionType.ONE_TO_MANY:
+            return ['1', 'n']
+        case connectionType.ONE_TO_ONE:
+            return ['1', '1']
+        default:
+            break
+    }
+}
